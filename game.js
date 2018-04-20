04409f32cb23050c5db4cdc39ae5b6a8b4e7be32
neilxcode <neiilis@live.com>
2018-04-20 11:38:25 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,10 +1,21 @@
+// IMAGE VARIABLES FOR MOVEMENT
+// speed = movement in pixels per second};
+var hero = { speed: 256 };
+var sword = { speed: 1024};
+var monster = { speed: 256};
+var monsterr = { speed: 256};
+var monsterr2 = { speed: 256};
+var monsterr3 = { speed: 256};
+var monstersCaught = 0;
+var keysDown = {};
+var level = 0;
+// var randomNum = Math.Round(Math.random());
+
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 1020;
 canvas.height = 960;
-var canvasX = canvas.width;
-var canvasY = canvas.height;
 document.body.appendChild(canvas);
 
 
@@ -18,8 +29,7 @@ bgImage.src = "img/Map.png";
 var bgX = bgImage.width;
 var bgY = bgImage.height;
 
-//ENTIRE BORDER
-
+//LEFT BORDER
 var leftBorderReady = false;
 var leftBorderImage = new Image();
 leftBorderImage.onload = function () {
@@ -27,6 +37,7 @@ leftBorderImage.onload = function () {
 }
 leftBorderImage.src = "img/leftTrees1020.png";
 
+//RIGHT BORDER
 var rightBorderReady = false;
 var rightBorderImage = new Image();
 rightBorderImage.onload = function () {
@@ -34,6 +45,7 @@ rightBorderImage.onload = function () {
 }
 rightBorderImage.src = "img/rightTrees1020.png";
 
+//TOP BORDER
 var upBorderReady = false;
 var upBorderImage = new Image();
 upBorderImage.onload = function () {
@@ -41,6 +53,7 @@ upBorderImage.onload = function () {
 }
 upBorderImage.src = "img/topTrees1020.png";
 
+//BOTTOM BORDER
 var downBorderReady = false;
 var downBorderImage = new Image();
 downBorderImage.onload = function () {
@@ -48,7 +61,7 @@ downBorderImage.onload = function () {
 }
 downBorderImage.src = "img/bottomTrees1020.png";
 
-// Hero image
+// HERO 
 var heroReady = false;
 var heroImage = new Image();
 heroImage.onload = function () {
@@ -56,7 +69,7 @@ heroImage.onload = function () {
 };
 heroImage.src = "img/avatarFaceDown.png";
 
-// Monster image
+// MONSTER //STOCK IMAGE
 var monsterReady = false;
 var monsterImage = new Image();
 monsterImage.onload = function () {
@@ -64,7 +77,7 @@ monsterImage.onload = function () {
 };
 monsterImage.src = "img/monster.png";
 
-// Monster2 image
+// MONSTERR1
 var monsterrReady = false;
 var monsterrImage = new Image();
 monsterrImage.onload = function () {
@@ -72,6 +85,7 @@ monsterrImage.onload = function () {
 };
 monsterrImage.src = "img/monsterr.png";
 
+//MONSTERR2
 var monsterr2Ready = false;
 var monsterr2Image = new Image();
 monsterr2Image.onload = function () {
@@ -79,6 +93,7 @@ monsterr2Image.onload = function () {
 };
 monsterr2Image.src = "img/monsterr2.png";
 
+//MONSTERR3
 var monsterr3Ready = false;
 var monsterr3Image = new Image();
 monsterr3Image.onload = function () {
@@ -86,130 +101,13 @@ monsterr3Image.onload = function () {
 };
 monsterr3Image.src = "img/monsterr3.png";
 
-// Sword image
+// SWORD
 var swordReady = false;
 var swordImage = new Image();
 swordImage.onload = function () {
 	swordReady = true;
 };
 swordImage.src = "img/ironSword.png";
-
-// Rock Barrier image
-var rockReady = false;
-var rockImage = new Image();
-rockImage.onload = function () {
-	rockReady = true;
-}
-rockImage.src = "img/rock_tall2.png";
-
-var theBadGuys = [];
-var spawnX, spawnY; 
-
-// var TO_RADIANS = Math.PI/180; 
-// function drawRotatedImage(image, x, y, angle)
-// { 
-//     // save the current co-ordinate system 
-//     // before we screw with it
-//     context.save(); 
-
-//     // move to the middle of where we want to draw our image
-//     context.translate(x, y);
-
-//     // rotate around that point, converting our 
-//     // angle from degrees to radians 
-//     context.rotate(angle * TO_RADIANS);
-
-//     // draw it up and to the left by half the width
-//     // and height of the image 
-//     context.drawImage(image, -(image.width/2), -(image.height/2));
-
-//     // and restore the co-ords to how they were when we began
-//     context.restore(); 
-// }
-
-
-// Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
-};
-var sword = {
-	speed: 1024
-}
-var monster = {
-	speed: 256
-};
-var monsterr = {
-	speed: 256
-};
-var monsterr2 = {
-	speed: 256
-};
-var monsterr3 = {
-	speed: 256
-};
-var monstersCaught = 0;
-
-var leftBorder = {};
-
-var rightBorder = {};
-
-var upBorder = {};
-
-var downBorder = {};
-
-// Handle keyboard controls
-var keysDown = {};
-
-
-// var monsterX = monster.x;
-// var monsterY = monster.y;
-// var monsterrX = monsterr.x;
-// var monsterrY = monsterr.y;
-
-// class Sword {
-// 	constructor(image, width, height, monsterx, monstery, monsterrx, monsterry, speed) {
-// 		this.height = height;
-// 		this.width = width;
-// 		this.image.src = "img/ironSword";
-// 	}
-// 	get collision() {
-// 		return this.swordCollision();
-// 	}
-// 	swordPosition() {
-// 		if (
-// 		this.width <= (monsterX + 40)
-// 		&& monsterX <= (this.width + 40)
-// 		&& this.height <= (monsterY + 40)
-// 		&& monsterY <= (this.height + 40)
-// 	) {
-		
-// 		++monstersCaught;
-// 		reset();
-
-// 	}
-
-
-// 	if (
-// 		this.width <= (monsterrX + 40)
-// 		&& monsterrX <= (this.width + 40)
-// 		&& this.height <= (monsterrY + 40)
-// 		&& monsterrY <= (this.height + 40)
-// 	) {
-// 		++monstersCaught;
-// 		reset();
-// 	}
-// 		return this.image, this.width, this.height, this.monsterX, this.monsterY, this.monsterrX, this.monsterrY, this.speed
-// 	}
-// }
-
-// // try const
-// const swordHit = new Sword(image.x, swordImage.height, monster.x, monster.y, monsterr.x, monsterr.y, 1024);
-
-
-
-
-
-// var randomNum = Math.Round(Math.random());
 
 // var monsterMovement = function(modifier){
 // 		for(i = 0; i <= randomNum; i++){
@@ -262,11 +160,10 @@ addEventListener("keyup", function (e) {
 // 		c.fill();
 // 	});
 // }
-
-
 	//TEST
 
-// Reset the game when the player catches a monster
+
+// RESETS GAME
 var reset = function () {
 
 	hero.x = (canvas.width / 2) - 32;
@@ -288,13 +185,12 @@ var reset = function () {
 	// Throw the monsterr somewhere on the screen randomly
 	monsterr3.x = 32 + (Math.random() * (canvas.width - 64));
 	monsterr3.y = 32 + (Math.random() * (canvas.height - 64));
-
-	// POSITION ROCK BARRIER
 };
 
-// Update game objects
+// UPDATE GAME OBJECTS
 var update = function (modifier) {
 
+	//HERO MOVEMENT MODIFIER
 	if (38 in keysDown) { // Player holding up
 		hero.y -= hero.speed * modifier;
 		sword.x = (hero.x + 44);
@@ -321,6 +217,7 @@ var update = function (modifier) {
 	}
 
 
+	//SWORD MOVEMENT MODIFIER
 	if (87 in keysDown) { //Player holding w
 		sword.y -= sword.speed * modifier;
 	}
@@ -337,7 +234,7 @@ var update = function (modifier) {
 		
 	}
 
-	// TOUCHING BORDER?
+	//PUSH HERO BACK IF TOUCHING BORDERS
 	if ((hero.x + 32) >= (canvas.width / 2) + 478){
 		hero.x = hero.x - 32;
 	}
@@ -350,46 +247,51 @@ var update = function (modifier) {
 	if ((hero.y + 128) <= (canvas.height) - 30){
 		hero.y = hero.y + 64;
 	}
-
-
-
-	// Are they touching?
-	if (
-		sword.x <= (monster.x + 40)
-		&& monster.x <= (sword.x + 40)
-		&& sword.y <= (monster.y + 40)
-		&& monster.y <= (sword.y + 40)
-	) {
-		
-		++monstersCaught;
-		reset();
-
-	}
-
-
-	if (
-		sword.x <= (monsterr.x + 40)
-		&& monsterr.x <= (sword.x + 40)
-		&& sword.y <= (monsterr.y + 40)
-		&& monsterr.y <= (sword.y + 40)
-	) {
-		++monstersCaught;
-		reset();
-	}
-	//left border
-
 	
-	// while(
-	// 	hero.y <= 
-	// &&  rockTopBorder.y <= (hero.y + 32)
-	// ) {
-	// 	hero.y = hero.y + 32;
-	// }
+	//ADD POINT AND RESET IF MONSTER CAUGHT
+		if (
+			sword.x <= (monster.x + 40)
+			&& monster.x <= (sword.x + 40)
+			&& sword.y <= (monster.y + 40)
+			&& monster.y <= (sword.y + 40)
+		) {
+			++monstersCaught;
+			reset();
+		}
+		//ADD POINT AND RESET IF MONSTERR CAUGHT
+		if (
+			sword.x <= (monsterr.x + 40)
+			&& monsterr.x <= (sword.x + 40)
+			&& sword.y <= (monsterr.y + 40)
+			&& monsterr.y <= (sword.y + 40)
+		) {
+			++monstersCaught;
+			reset();
+		}
+		//ADD POINT AND RESET IF MONSTERR2 CAUGHT
+		if (
+			sword.x <= (monsterr2.x + 40)
+			&& monsterr2.x <= (sword.x + 40)
+			&& sword.y <= (monsterr2.y + 40)
+			&& monsterr2.y <= (sword.y + 40)
+		) {
+			++monstersCaught;
+			reset();
+		}
+		//ADD POINT AND RESET IF MONSTERR3 CAUGHT
+		if (
+			sword.x <= (monsterr3.x + 40)
+			&& monsterr3.x <= (sword.x + 40)
+			&& sword.y <= (monsterr3.y + 40)
+			&& monsterr3.y <= (sword.y + 40)
+		) {
+			++monstersCaught;
+			reset();
+		}
 
-	
 };
 
-// Draw everything
+// DRAW OBJECTS
 var render = function () {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
@@ -434,34 +336,38 @@ var render = function () {
 		ctx.drawImage(rightBorderImage, 988, 0);
 	}
 
-	// Score
+	//MONSTERS CAUGHT
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "20px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+
+
+	if(monstersCaught >= 10){
+		level + 1;
+		ctx.fillStyle = "rgb(250, 250, 250)";
+		ctx.font = "20px Helvetica";
+		ctx.textAlign = "left";
+		ctx.textBaseline = "top";
+		ctx.fillText("Level 2", 900, 32);
+	}
 };
 
-// The main game loop
+
+//MAIN GAME LOOP.ORDER OF FUNCTIONS
 var main = function () {
 	//NOW = TIME AT START
 	var now = Date.now();
 	//DELTA = TIME AT START - TIME ELAPSED
 	var delta = now - then;
-	//CALLS MODIFIER FOR MOVEMENT
+	//CALLS MODIFIER FOR DEFINING X,Y POSITIONS
 	update(delta / 1000);
-	// swordHit();
-	// update(delta / 1000);
-
-	// playerMove();		
-	// playerDraw();
-
-
 	//CALL RENDER TO DRAW
 	render();
-	//RESETS CLOCK(loops for constant animation)
+	//RESETS CLOCK START CLOCK
 	then = now;
-	// Request to do this again ASAP(loop for constant animation)
+	//REQUEST TO RUN MAIN AGAIN
 	requestAnimationFrame(main);
 };
 
@@ -469,8 +375,9 @@ var main = function () {
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-// Let's play this game!
 //THEN = TIME AT START
 var then = Date.now();
+//CALL RESET
 reset();
+//CALL MAIN
 main();
